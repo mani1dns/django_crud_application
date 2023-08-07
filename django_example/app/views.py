cc7d86c1872c4854ffd9e6e97b1ad95cd241df0a
Mani A <mani.msd1357@gmail.com>
2023-08-07 20:05:10 +0530

    setting primary key but it is not necessary, already it will be set as primary key

--- django_example/app/views.py
+++ django_example/app/views.py
@@ -7,3 +7,14 @@ def index(request):
 
 def about(request):
     return render(request, "about.html")
+
+def insertData(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        age=request.POST.get('age')
+        gender=request.POST.get('gender')
+        print(name, email, age, gender)
+
+
+    return render(request, "index.html")
