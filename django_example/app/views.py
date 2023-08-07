12f98e75997b6c6fcf081873ea8dcdae5434246a
Mani A <mani.msd1357@gmail.com>
2023-08-07 21:01:26 +0530

    admin updated

--- django_example/app/views.py
+++ django_example/app/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
-
-def about(request):
-    return render(request, "about.html")
 
 def insertData(request):
     if request.method=="POST":
@@ -14,7 +11,10 @@ def insertData(request):
         email=request.POST.get('email')
         age=request.POST.get('age')
         gender=request.POST.get('gender')
-        print(name, email, age, gender)
-
-
+        print(name,email,age,gender)
+        query=Student(name=name,email=email,age=age,gender=gender)
+        query.save()
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
